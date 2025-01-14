49a974e22dd97d2df2cb1c1904b6dd341601905e
İbrahim TULUMCU <94108626+ibrahimTlmcu@users.noreply.github.com>
2025-01-14 14:39:10 +0300

    CategoryService

---
+++ Services/Catalog/Ecommerce.Catalog/Services/CategoryServices/CategoryService.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using Ecommerce.Catalog.Dtos.CategoryDtos;
+using Ecommerce.Catalog.Entities;
+using Ecommerce.Catalog.Settings;
+using MongoDB.Driver;
+
+namespace Ecommerce.Catalog.Services.CategoryServices
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly IMongoCollection<Category> _categoryCollection;
+
+        private readonly IMapper _mapper;
+
+        // Ilk adim Baglanti ---> Database ---> Tablo
+
+        public CategoryService(IMapper mapper , IDatabaseSettings _databaseSettings)
+        {
+            var client = new MongoClient(_databaseSettings.ConnectionString);///app.json icine _dbsetting ile eristik
+            var database = client.GetDatabase(_databaseSettings.DatabaseName);//database eristik
+            _categoryCollection = database.GetCollection<Category>(_databaseSettings.CategoryCollectionName);//tabloya ersitik
+            _mapper = mapper;
+
+        }
+        public async Task CreateCategoryAsync(CreateCategoryDto createCategoryDto)
+        {
+            var value = _mapper.Map<Category>(createCategoryDto);
+            await 
+        }
+
+        public Task DeleteCategoryAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetByIdCategoryDto> GetByIdCategory(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultCategoryDto>> GettAllCategoryAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/Catalog/Ecommerce.Catalog/Services/CategoryServices/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using Ecommerce.Catalog.Dtos.CategoryDtos;
+
+namespace Ecommerce.Catalog.Services.CategoryServices
+{
+    //Ekle sil Guncelle getir gibi islemlerin metotlarini tutacak
+    //CategoryService clasinin icini bu metotlarla dolduracagiz.
+    public interface ICategoryService
+    {
+        Task<List<ResultCategoryDto>> GettAllCategoryAsync();
+        Task CreateCategoryAsync(CreateCategoryDto createCategoryDto);
+        Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto);
+        Task DeleteCategoryAsync(string id);
+        Task <GetByIdCategoryDto>  GetByIdCategory(string id);
+     }
+}
