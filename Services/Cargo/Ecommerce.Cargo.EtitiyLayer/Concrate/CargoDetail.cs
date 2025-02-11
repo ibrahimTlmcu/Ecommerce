ad543e1b0b2ceb5d91aa01fde1d5fde6b9c16d3f
İbrahim TULUMCU <94108626+ibrahimTlmcu@users.noreply.github.com>
2025-02-11 12:58:35 +0300

    CargoDetail

---
+++ Services/Cargo/Ecommerce.Cargo.EtitiyLayer/Concrate/CargoDetail.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Cargo.EtitiyLayer.Concrate
+{
+    public class CargoDetail
+    {
+    }
+}
