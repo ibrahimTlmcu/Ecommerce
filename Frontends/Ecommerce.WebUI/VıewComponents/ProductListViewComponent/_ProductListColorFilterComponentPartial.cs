81e9e22cc53652729d3c8e0f14171a517be37028
İbrahim TULUMCU <94108626+ibrahimTlmcu@users.noreply.github.com>
2025-02-12 20:32:22 +0300

    Filtreleme Islemleri

---
+++ Frontends/Ecommerce.WebUI/VıewComponents/ProductListViewComponent/_ProductListColorFilterComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.WebUI.VıewComponents.ProductListViewComponent
+{
+    public class _ProductListColorFilterComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
