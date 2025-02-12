82ba081af7ab5078bebe23c1467f00457d067206
İbrahim TULUMCU <94108626+ibrahimTlmcu@users.noreply.github.com>
2025-02-12 14:36:50 +0300

    UI

---
+++ Frontends/Ecommerce.WebUI/VıewComponents/UILayoutViewComponents/_HeadUILayoutComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.WebUI.VıewComponents.UILayoutViewComponents
+{
+    public class _HeadUILayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
