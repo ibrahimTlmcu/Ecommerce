c9d8b6bc464955ec1c03aff95f47500b0ff58141
İbrahim TULUMCU <94108626+ibrahimTlmcu@users.noreply.github.com>
2025-02-20 17:00:06 +0300

    OfferDiscount Controller added

---
+++ Services/Catalog/Ecommerce.Catalog/Services/OfferDiscountService/IOfferDiscountService.cs
@@ -0,0 +1,13 @@
+﻿using Ecommerce.Catalog.Dtos.OfferDiscountDtos;
+
+namespace Ecommerce.Catalog.Services.OfferDiscountService
+{
+    public interface IOfferDiscountService
+    {
+        Task<List<ResultOfferDiscountDto>> GettAllOfferDiscountAsync();
+        Task CreateOfferDiscountAsync(CreateOfferDiscountDto createOfferDiscountDto);
+        Task UpdateOfferDiscountAsync(UpdateOfferDiscountDto updateOfferDiscountDto);
+        Task DeleteOfferDiscountAsync(string id);
+        Task<GetByIdOfferDiscountDto> GetByIdOfferDiscount(string id);
+    }
+}
---
+++ Services/Catalog/Ecommerce.Catalog/Services/OfferDiscountService/OfferDiscountService.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using Ecommerce.Catalog.Dtos.OfferDiscountDtos;
+using Ecommerce.Catalog.Entities;
+using Ecommerce.Catalog.Settings;
+using MongoDB.Driver;
+
+namespace Ecommerce.Catalog.Services.OfferDiscountService
+{
+    public class OfferDiscountService : IOfferDiscountService
+    {
+        private readonly IMongoCollection<OfferDiscount> _OfferDiscountCollection;
+
+        private readonly IMapper _mapper;
+
+        // Ilk adim Baglanti ---> Database ---> Tablo
+
+        public OfferDiscountService(IMapper mapper, IDatabaseSettings _databaseSettings)
+        {
+            var client = new MongoClient(_databaseSettings.ConnectionString);///app.json icine _dbsetting ile eristik
+            var database = client.GetDatabase(_databaseSettings.DatabaseName);//database eristik
+            _OfferDiscountCollection = database.GetCollection<OfferDiscount>(_databaseSettings.OfferDiscountsCollectionName);//tabloya ersitik
+            _mapper = mapper;
+
+        }
+        public async Task CreateOfferDiscountAsync(CreateOfferDiscountDto createOfferDiscountDto)
+        {
+            var value = _mapper.Map<OfferDiscount>(createOfferDiscountDto);
+            await _OfferDiscountCollection.InsertOneAsync(value);
+        }
+
+        public async Task DeleteOfferDiscountAsync(string id)
+        {
+            await _OfferDiscountCollection.DeleteOneAsync(x => x.OfferDiscountId == id);
+
+        }
+
+        public async Task<GetByIdOfferDiscountDto> GetByIdOfferDiscount(string id)
+        {
+            var values = await _OfferDiscountCollection.Find(x => x.OfferDiscountId == id).FirstOrDefaultAsync();
+            return _mapper.Map<GetByIdOfferDiscountDto>(values);
+        }
+
+        public async Task<List<ResultOfferDiscountDto>> GettAllOfferDiscountAsync()
+        {
+            var values = await _OfferDiscountCollection.Find(x => true).ToListAsync();
+            return _mapper.Map<List<ResultOfferDiscountDto>>(values);
+        }
+
+        public async Task UpdateOfferDiscountAsync(UpdateOfferDiscountDto updateOfferDiscountDto)
+        {
+            var values = _mapper.Map<OfferDiscount>(updateOfferDiscountDto);
+            await _OfferDiscountCollection.FindOneAndReplaceAsync(x => x.OfferDiscountId == updateOfferDiscountDto.OfferDiscountId, values);
+        }
+    }
+}
