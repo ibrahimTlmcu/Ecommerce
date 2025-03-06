684abdb84be3c50a9cb587345a015991fa29a1cc
İbrahim TULUMCU <94108626+ibrahimTlmcu@users.noreply.github.com>
2025-03-06 23:01:37 +0300

    Token ve  Login hatalari duzletildi

--- IdentityServer/Ecommerce.IdentityServer/Controllers/LoginController.cs
+++ IdentityServer/Ecommerce.IdentityServer/Controllers/LoginController.cs
@@ -1,5 +1,6 @@
 ﻿using Ecommerce.IdentityServer.Dtos;
 using Ecommerce.IdentityServer.Models;
+using Ecommerce.IdentityServer.Tools;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
@@ -14,9 +15,12 @@ namespace Ecommerce.IdentityServer.Controllers
     {
         private readonly SignInManager<ApplicationUser> _singInInManager;
 
-        public LoginController(SignInManager<ApplicationUser> singInInManager)
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public LoginController(SignInManager<ApplicationUser> singInInManager, UserManager<ApplicationUser> userManager)
         {
             _singInInManager = singInInManager;
+            _userManager = userManager;
         }
 
         [HttpPost]
@@ -25,9 +29,14 @@ namespace Ecommerce.IdentityServer.Controllers
             var result = await _singInInManager.PasswordSignInAsync(userLoginDto.UserName, userLoginDto.Password, false, false);
             //ilk false remember me anlamina gelir
             //kullanici sifreyi yuanlis girdighinde artar 5 oldunca da kilitler
+            var user = await _userManager.FindByNameAsync(userLoginDto.UserName);
             if (result.Succeeded)
             {
-                return Ok("Giris Islemi Basarili ! ");
+                GetCheckAppUserViewModel model = new GetCheckAppUserViewModel();
+                model.UserName = userLoginDto.UserName;
+                model.Id = user.Id;
+                var token = JwtTokenGenerator.GenerateToken(model);
+                return Ok(token);
             }
             else
             {
